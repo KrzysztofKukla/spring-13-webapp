b8de70b8cbeaec90fa6e072f3bfcf6416a0ad986
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-05-05 01:16:10 +0200

    Initial commit

---
+++ src/main/java/pl/kukla/krzys/webapp/DemoProjectForSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectForSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectForSpringBootApplication.class, args);
+	}
+
+}
